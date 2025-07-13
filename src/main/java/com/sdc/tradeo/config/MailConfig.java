ec3e179a34d4b7a470d8d82cb5c2518ade17bd44
Thulasidharan P <160030369+dharan-sdc@users.noreply.github.com>
2025-07-13 10:08:34 +0530

    Update MailConfig.java

--- src/main/java/com/sdc/tradeo/config/MailConfig.java
+++ src/main/java/com/sdc/tradeo/config/MailConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Properties;
 
