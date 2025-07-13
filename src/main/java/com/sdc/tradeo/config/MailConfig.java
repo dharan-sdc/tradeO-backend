714c07754b3bbe45d5efee24adfaa75a27f67f27
Thulasidharan P <160030369+dharan-sdc@users.noreply.github.com>
2025-07-13 09:57:45 +0530

    Update MailConfig.java

--- src/main/java/com/sdc/tradeo/config/MailConfig.java
+++ src/main/java/com/sdc/tradeo/config/MailConfig.java
@@ -10,13 +10,19 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+     @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+    
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("dharanpostmail@gmail.com");
-        mailSender.setPassword("xxru hqwz obuh peya");
+         mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
